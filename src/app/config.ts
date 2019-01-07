444e014a845a8a0a1cda430201967a4901279e8f
Ben Hernandez <benaychh@gmail.com>
2019-01-07 14:00:34 -0600

    Adding API to deployed site config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,16 +1,20 @@
 class Config {
   get apiUrl(): string {
-    switch (window.location.hostname) {
-      case 'localhost':
-        switch (window.location.port) {
-          case '4201':
-            return 'http://localhost:8001/v1/';
-          default:
-            return 'http://localhost:8000/v1/';
-        }
+    let apiUrl = 'http://localhost:8000/v1/';
+    if (window.location.hostname === 'localhost') {
+      apiUrl = handleLocalHost();
+    } else if (window.location.hostname === 'sa-todo-list.firebaseapp.com') {
+      apiUrl = 'https://todoapi.benaychh.io/v1';
     }
-    return 'http://localhost:8000/v1/';
+    return apiUrl;
   }
+}
+
+function handleLocalHost() {
+  if (window.location.port === '4201') {
+    return 'http://localhost:8001/v1/';
+  }
+  return 'http://localhost:8000/v1/';
 }
 
 export const config = new Config();
